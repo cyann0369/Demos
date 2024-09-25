491a0b60096f1a690811300a0ffd96b1b5625a09
julienRactM <julien.ract@sfr.fr>
2024-09-25 16:38:27 +0200

    [Update] Preprocessing & lm

--- data_analysis.R
+++ data_analysis.R
@@ -1,6 +1,7 @@
 library(readxl)
 library(reactable)
-# library(htmltools)
+
+library(caret)
 
 load_data <- function(file_path){
   data <- read_excel(file_path)
@@ -44,4 +45,43 @@ generate_reactable_table <- function(data, columns = "all", bar_height = "16px",
     data[, columns, drop = FALSE],  # Select only the specified columns
     columns = col_definitions
   )
+}
+
+# return a dataframe with scaled vars, if none are given, all columns of the dataframe will be scaled
+# usage example df_scaled <- min_max_scaling(data, c("HLM", "Salairemoy", "TxPauv", "NonDiplome", "txcho"))
+min_max_scaling <- function(df, columns_to_normalize = "all") {
+
+  # If columns_to_normalize is set to "all", use all numeric columns from the dataset
+  if (identical(columns_to_normalize, "all")) {
+    columns_to_normalize <- names(df)[sapply(df, is.numeric)]
+  }
+
+  # Apply preProcess to the selected columns
+  process <- preProcess(df[columns_to_normalize], method = c("range"))
+
+  # Predict (normalize) the columns
+  norm_columns <- predict(process, df[columns_to_normalize])
+
+  # Replace the original columns with the normalized columns
+  df[columns_to_normalize] <- norm_columns
+
+  # Return the dataframe with scaled columns replaced
+  return(df)
+}
+
+
+# Function to split data into train and test sets
+train_test_split <- function(df, target_column = "txabs", p = 0.8) {
+  # Set seed for reproducibility
+  set.seed(123)
+
+  # Create the training index
+  train_index <- createDataPartition(df[[target_column]], p = p, list = FALSE)
+
+  # Split the data into training and test sets
+  train_data <- df[train_index, ]
+  test_data <- df[-train_index, ]
+
+  # Return a list containing both train and test sets
+  return(list(train = train_data, test = test_data))
 }
